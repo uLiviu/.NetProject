6fe98d18fff91046f3c6b6e74967d334ae5e46b5
uLiviu <liviu.ungureanu@isa.utm.md>
2023-02-26 11:01:23 +0200

    Editare

---
+++ Project/eUseControl/eUseControl.Web/BundleConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace eUseControl.Web
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundels(BundleCollection bundles)
+        {
+            //Vendor CSS Bundle
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/bootstrap-icons/css").Include(
+                "~/Content/vendor/bootstrap-icons/bootstrap-icons.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/boxicons/css").Include(
+                "~/Content/vendor/boxicons/boxicons.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/quill.snow/css").Include(
+                "~/Content/vendor/quill/quill.snow.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/quill.bubble/css").Include(
+                "~/Content/vendor/quill/quill.bubble.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/remixicon/css").Include(
+                "~/Content/vendor/remixicon/remixicon.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/simple-datatables/css").Include(
+                "~/Content/vendor/simple-datatables/style.css", new CssRewriteUrlTransform()));
+            //Template Main CSS File
+            bundles.Add(new StyleBundle("~/bundles/main/css").Include(
+                "~/Content/css/dark-style.css", new CssRewriteUrlTransform()));
+
+            //Vendor JS bundle
+            bundles.Add(new ScriptBundle("~/bundles/apexcharts/js").Include(
+                "~/Scripts/vendor/apexcharts/apexcharts.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(
+                "~/Scripts/bootstrap.bundle.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/chart/js").Include(
+                "~/Scripts/vendor/chart.js/chart.umd.js"));
+            bundles.Add(new ScriptBundle("~/bundles/echarts/js").Include(
+                "~/Scripts/vendor/echarts/echarts.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/quill/js").Include(
+                "~/Scripts/vendor/quill/quill.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/simple-datatables/js").Include(
+                "~/Scripts/vendor/simple-datatables/simple-datatables.js"));
+            bundles.Add(new ScriptBundle("~/bundles/tinymce/js").Include(
+                "~/Scripts/vendor/tinymce/tinymce.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/validate/js").Include(
+                "~/Scripts/vendor/php-email-form/validate.js"));
+
+            //Template Main JS File
+            bundles.Add(new ScriptBundle("~/bundles/main/js").Include(
+                "~/Scripts/js/main.js"));
+        }
+    }
+}
---
+++ Project/eUseControl/eUseControl.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eUseControl.Web.Models;
+
+namespace eUseControl.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            UserData u = new UserData();
+            u.UserName = "Customer";
+            u.Products = new List<string> { "Product #1", "Product #2", "Product #3", "Product #4" };
+
+            return View(u);
+        }
+
+        public ActionResult Product()
+        {
+            var product = Request.QueryString["p"];
+
+            UserData u = new UserData();
+            u.UserName = "Customer";
+            u.SingleProduct = product;
+
+            return View(u);
+        }
+
+        //http://localhost:31843/Home/Product?p=JORA
+        [HttpPost]
+        public ActionResult Product(string btn)
+        {
+            return RedirectToAction("Product", "Home", new { @p = btn });
+        }
+    }
+}
